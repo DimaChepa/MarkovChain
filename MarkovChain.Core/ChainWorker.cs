1ecc4880e587f79cc7008cc11cbe642b282ba107
DimaChepa <dmitriiyazz@gmail.com>
2018-01-28 19:18:56 +0200

    Add skeleton of markov chain

---
+++ MarkovChain.Core/ChainWorker.cs
@@ -0,0 +1,28 @@
+﻿namespace MarkovChain.Core
+{
+    using System.Collections.Generic;
+    public class ChainWorker
+    {
+        private IList<double> _data;
+        private IList<Model> _predictSet;
+        public ChainWorker(IList<double> data)
+        {
+            _data = data;
+        }
+
+        public void Activate()
+        {
+
+        }
+
+        public void Calulate()
+        {
+
+        }
+
+        public IList<Model> GetData()
+        {
+            return _predictSet;
+        }
+    }
+}
---
+++ MarkovChain.Core/Model.cs
@@ -0,0 +1,36 @@
+﻿namespace MarkovChain.Core
+{
+    public class Model
+    {
+        private bool _isCurrentPosition;
+        private bool _isBottomPosition;
+        private bool _isTopPosition;
+        public Model(bool isCurrentPostion, bool isBottomPostion, bool isTopPostion)
+        {
+            _isCurrentPosition = isCurrentPostion;
+            _isBottomPosition = isBottomPostion;
+            _isTopPosition = isTopPostion;
+        }
+        public bool IsCurrentPosition
+        {
+            get
+            {
+                return _isCurrentPosition;
+            }
+        }
+        public bool IsBottomPosition
+        {
+            get
+            {
+                return _isBottomPosition;
+            }
+        }
+        public bool IsTopPosition
+        {
+            get
+            {
+                return _isTopPosition;
+            }
+        }
+    }
+}
