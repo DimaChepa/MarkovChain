59b63160356e3444beffb129e2a3366ac82b6365
DimaChepa <dmitriiyazz@gmail.com>
2018-01-28 20:56:41 +0200

    Add type of transition for each element

--- MarkovChain.Console/Program.cs
+++ MarkovChain.Console/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace MarkovChain.Console
 {
+    using MarkovChain.Core;
     using System;
+    using System.Collections.Generic;
     class Program
     {
         static void Main(string[] args)
         {
+            List<double> list = new List<double>() { 1, 2, 3, 4, 5 };
+            ChainWorker worker = new ChainWorker(list);
+            worker.Activate();
             Console.WriteLine("Hello World!");
         }
     }
--- MarkovChain.Core/ChainWorker.cs
+++ MarkovChain.Core/ChainWorker.cs
@@ -12,10 +12,26 @@
 
         public void Activate()
         {
-
+            _predictSet = new List<Model>();
+            _predictSet.Add(new Model(true, false, false));
+            for (int i = 1; i < _data.Count; i++)
+            {
+                if (_data[i] > _data[i - 1])
+                {
+                    _predictSet.Add(new Model(false, false, true));
+                }
+                else if (_data[i] < _data[i - 1])
+                {
+                    _predictSet.Add(new Model(false, true, false));
+                }
+                else
+                {
+                    _predictSet.Add(new Model(true, false, false));
+                }
+            }
         }
 
-        public void Calulate()
+        public void Calculate()
         {
 
         }
