a27dea56c7346bfd9e5f04479e27c37f7c25dbfa
DimaChepa <dmitriiyazz@gmail.com>
2018-01-28 17:33:02 +0200

    Add console app project

---
+++ MarkovChain.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MarkovChain.Console
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
